e7110f5c74067c42e743026c1ddd6b3b032f978a
Mloong <164481121@qq.com>
2018-10-09 11:50:24 +0800

    Initial commit

---
+++ src/test/java/com/themeleaf/ThemeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.themeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThemeleafApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
